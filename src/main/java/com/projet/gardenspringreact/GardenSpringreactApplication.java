7030819a95f13ef3754883d5ae06f173346a20ff
JulieStevenin <140088946+JulieStevenin@users.noreply.github.com>
2023-09-20 10:41:36 +0200

    Premier ajout test

---
+++ src/main/java/com/projet/gardenspringreact/GardenSpringreactApplication.java
@@ -0,0 +1,13 @@
+package com.projet.gardenspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenSpringreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenSpringreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/gardenspringreact/GardenSpringreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.gardenspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenSpringreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
